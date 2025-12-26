d5964fb0f7f5c384aa5850f5a9366c79e152c2a4
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-12-26 18:55:13 +0700

    init

--- lib/1_domain/repositories/advice_repo.dart
+++ lib/1_domain/repositories/advice_repo.dart
@@ -1,5 +1,5 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture/1_domain/entities/advice_entity.dart';
+import 'package:dartz/dartz.dart';
 
 import '../failures/failures.dart';
 
--- lib/1_domain/usecases/advice_usecases.dart
+++ lib/1_domain/usecases/advice_usecases.dart
@@ -2,14 +2,13 @@ import 'package:flutter_clean_architecture/1_domain/entities/advice_entity.dart'
 import 'package:flutter_clean_architecture/1_domain/failures/failures.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../0_data/repositories/advice_repo_impl.dart';
+
 class AdviceUseCases {
+  final adviceRepo = AdviceRepoImpl();
+
   Future<Either<Failure, AdviceEntity>> getAdvice() async {
-    // call a repository to get data (failure or data)
-    // proceed with business logic (manipulate the data)
-    await Future.delayed(const Duration(seconds: 3), () {});
-    // call to repo went good:  -> return data not failure
-    // return right(const AdviceEntity(advice: 'advice to test', id: 1));
-    // call to repo went bad or logic had an error -> return failure (left side)
-    return left(CacheFailure());
+    return adviceRepo.getAdviceFromDatasource();
+    // space for business logic
   }
 }
